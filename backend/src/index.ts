d176487ad34a52dd03111c6082b5c13638333253
heppaat <pali.herrer@gmail.com>
2024-06-29 19:39:17 +0200

    backend saveFile and loadFile function implemented in util

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world")
+import express from "express";
+import cors from "cors";
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
---
+++ backend/src/util/db.ts
@@ -0,0 +1,33 @@
+import fs from "fs/promises";
+import { unknown, z } from "zod";
+
+export const load = async <Schema extends z.ZodTypeAny>(
+  filename: string,
+  schema: Schema
+): Promise<z.infer<typeof schema> | null> => {
+  try {
+    const path = `${__dirname}/../../database/${filename}.json`;
+    const rawData = await fs.readFile(path, "utf-8");
+    const data = JSON.parse(rawData);
+    const validatedData = schema.parse(data);
+    return validatedData;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const save = async <Schema extends z.ZodTypeAny>(
+  filename: string,
+  data: unknown,
+  schema: Schema
+): Promise<{ success: boolean }> => {
+  try {
+    const path = `${__dirname}/../../database${filename}.json`;
+    const dataToInsert = schema.parse(data);
+    const fileContent = JSON.stringify(dataToInsert, null, 2);
+    await fs.writeFile(path, fileContent);
+    return { success: true };
+  } catch (error) {
+    return { success: false };
+  }
+};
