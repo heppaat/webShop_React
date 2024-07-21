89e855f34552441fb919c1421a46adf7bd48836f
heppaat <pali.herrer@gmail.com>
2024-07-21 21:21:15 +0200

    backend delete request implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { load, save } from "./util/db";
 import { ItemSchema } from "./modell";
+import { z } from "zod";
 
 const server = express();
 
@@ -46,17 +47,71 @@ server.post("/api/bag", async (req, res) => {
       return item;
     });
 
-    const isSuccessful = await save("bag", updatedBagItems, ItemSchema.array());
-    if (!isSuccessful) return res.sendStatus(500);
+    const isSuccessfull = await save(
+      "bag",
+      updatedBagItems,
+      ItemSchema.array()
+    );
+    if (!isSuccessfull) return res.sendStatus(500);
     res.json({ success: true });
   } else {
     validatedNewItem.counter = 1;
 
     const newBagItems = [...bagItems, { ...validatedNewItem }];
 
-    const isSuccessful = await save("bag", newBagItems, ItemSchema.array());
-    if (!isSuccessful) return res.sendStatus(500);
+    const isSuccessfull = await save("bag", newBagItems, ItemSchema.array());
+    if (!isSuccessfull) return res.sendStatus(500);
     res.json({ success: true });
+  }
+});
+
+server.delete("/api/bag/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = z.coerce.number().safeParse(id);
+
+  if (!result.success) return res.sendStatus(400).json(result.error.issues);
+
+  const itemToDeleteId = result.data;
+
+  const bagItems = await load("bag", ItemSchema.array());
+  if (!bagItems) return res.sendStatus(500);
+
+  const existingItem = bagItems.find((item) => item.id === itemToDeleteId);
+
+  if (existingItem) {
+    if (existingItem.counter > 1) {
+      const updatedBagItems = bagItems.map((item) => {
+        if (item.id === existingItem.id) {
+          return {
+            ...item,
+            counter: item.counter - 1,
+            price: item.price - item.originalPrice,
+          };
+        }
+        return item;
+      });
+      const isSuccessfull = await save(
+        "bag",
+        updatedBagItems,
+        ItemSchema.array()
+      );
+      if (!isSuccessfull) return res.sendStatus(500);
+      res.json({ success: true });
+    } else {
+      const filteredItems = bagItems.filter(
+        (item) => item.id !== itemToDeleteId
+      );
+
+      const isSuccessfull = await save(
+        "bag",
+        filteredItems,
+        ItemSchema.array()
+      );
+      if (!isSuccessfull) return res.sendStatus(500);
+      res.json({ success: true });
+    }
+  } else {
+    return res.sendStatus(500);
   }
 });
 
